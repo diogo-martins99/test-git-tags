5f49f77a88e8feefb49a125a58b896b4f400a193
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-04-26 15:55:22 +0100

    lol

--- TestProjectForGit/Program.cs
+++ TestProjectForGit/Program.cs
@@ -17,7 +17,7 @@ namespace TestProjectForGit
         // And this is an instance method
         private void PrintSomething()
         {
-            Console.WriteLine("I'm a test for git tags");
+            Console.WriteLine("new one");
             Console.WriteLine("Tags are a good way to remember versions");
             Console.ReadKey();
         }
